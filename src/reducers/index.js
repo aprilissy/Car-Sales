78c8d083f2717ffac222898d4fa85f317b3c5527
April Darger <april.m.darger@gmail.com>
2020-11-03 16:03:26 -0700

    Set up store and Provider. Created index.js in reducers folder to handle combining the reducers to be passed into createStore() in src/index.js.

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux'
+import featuresReducer from './featuresReducer'
+import totalReducer from './totalReducer'
+
+export default combineReducers({featuresReducer, totalReducer})
